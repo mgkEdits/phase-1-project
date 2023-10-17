cbdfa7a05b1293d9a191fca78655cb5e35a4e13d
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-10-17 08:34:07 +0300

    completed project

---
+++ README.md
@@ -0,0 +1,92 @@
+/*   Front-End by mgkEdits 
+     Data by F1
+     Using locally generated f1 json file  */
+
+## phase-1-project
+
+# Formula 1 Competition Statistics Website
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Configuration](#configuration)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+Welcome to the Formula 1 Competition Statistics Website! This website utilizes an F1 data API to provide comprehensive statistics and information about Formula 1 races, drivers, teams, and more. Whether you're an avid F1 fan or just interested in exploring the world of Formula 1, this website is designed to offer a user-friendly and informative experience.
+
+## Features
+
+- **Race Information**: Access detailed information about each race, including date, location, and circuit details.
+
+- **Driver Statistics**: Explore statistics about F1 drivers, including their performance in various races.
+
+- **Team Standings**: Get up-to-date information on team standings, points, and rankings.
+
+- **Fastest Lap**: Find out who achieved the fastest lap in a specific race and more.
+
+- **Slick User Interface**: A responsive and user-friendly design for seamless navigation.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- **Web Browser**: A modern web browser such as Google Chrome, Mozilla Firefox, or Microsoft Edge.
+
+## Getting Started
+
+### Installation
+
+1. Clone the repository to your local machine using the following command:
+
+   ```
+   git clone https://github.com/yourusername/your-repo.git
+   ```
+
+2. Open the project folder in your code editor of choice.
+
+### Configuration
+
+1. Activate the json-server to watch the db.json file.
+
+   ```
+   phase-1-project$~ json-server --watch db.json
+   ```
+
+## Usage
+
+1. Launch the website by opening the project folder in your code editor and running the following command:
+
+
+2. Open the index.html document and run with live server to access the Formula 1 Competition Statistics Website.
+
+3. Explore the website's various features, including race information, driver statistics, team standings, and more.
+
+## Contributing
+
+Contributions are always welcome! Whether it's reporting issues, suggesting improvements, or submitting pull requests, your help is appreciated.
+
+To contribute to this project:
+
+1. Fork the repository on GitHub.
+
+2. Create a new branch with a descriptive name.
+
+3. Make your changes and commit them.
+
+4. Push your changes to your branch.
+
+5. Submit a pull request to the main repository.
+
+## License
+
+This project is licensed under the free to use License. See the [LICENSE](LICENSE) file for more details.
+
+---
---
+++ src/index.js
@@ -0,0 +1,312 @@
+const init = () => {
+
+    //elements to be got from DOM
+    const trackCircuits2023 = document.getElementById('trackCircuits');
+    const sectionTitle = document.getElementById('sectionHeader')
+
+    //function to retrieve races
+    function getRaces() {
+      fetch(`http://localhost:3000/race`)
+        .then(res => res.json())
+        .then(data => {
+          // Get F1 race track data
+          trackCircuits2023.innerHTML = "";
+          data.forEach(track => {
+            console.log(track);
+            const F1TrackCard = createTrackCircuitCards(track);
+            trackCircuits2023.appendChild(F1TrackCard);
+  
+            // Add an event listener to the "View Track" button inside the card
+            const viewTrackBtn = F1TrackCard.querySelector('.btn');
+            viewTrackBtn.addEventListener('click', (event) => {
+              event.preventDefault();
+              handleClickViewCircuit(track);
+              
+            });
+          });
+        })
+    }
+
+    //event listener for F1 teams
+    document.getElementById('f1Teams').addEventListener('click', (event) =>{
+      event.preventDefault();
+      fetch(`http://localhost:3000/teams`)
+        .then(res => res.json())
+        .then(data => {
+          // Get F1 Teams data
+          sectionTitle.textContent = `Teams`;
+          sectionTitle.setAttribute('data-title', '2023 F1 Teams');
+          trackCircuits2023.innerHTML = "";
+          data.forEach(team => {
+              
+            const F1TrackCard = createTeamCards(team);
+            trackCircuits2023.appendChild(F1TrackCard);
+  
+            //tertiary functionality
+            // // Add an event listener to the "View Track" button inside the card
+            // const viewTrackBtn = F1TrackCard.querySelector('.btn');
+            // viewTrackBtn.addEventListener('click', (event) => {
+            //   event.preventDefault();
+            //   handleClickViewCircuit(track);
+            // });
+          });
+        })
+    } )
+
+    //event listener for F1 Standings
+    document.getElementById('f1Standings').addEventListener('click', (event) =>{
+      event.preventDefault();
+      fetch(`http://localhost:3000/constructorsStandings`)
+        .then(res => res.json())
+        .then(data => {
+          const table = document.createElement('table');
+          table.className = "styled-table";
+          const thead = table.createTHead();
+          const tbody = table.createTBody();
+
+          // Create table header row
+          const headerRow = thead.insertRow();
+          const headerPositions = ["Position", "Team Name", "Points"];
+
+          headerPositions.forEach((headerText) => {
+            const th = document.createElement('th');
+            th.textContent = headerText;
+            headerRow.appendChild(th);
+          });
+ 
+          // Create table rows for each team
+          data.forEach((team) => {
+            const row = tbody.insertRow();
+            const rowData = [team.position, team.teamName, team.points];
+
+            rowData.forEach((value) => {
+              const cell = row.insertCell();
+              cell.textContent = value;
+            });
+          });
+
+          sectionTitle.textContent = `Constructors Championship`;
+          sectionTitle.setAttribute('data-title', '2023 F1 Team Standing');
+
+          table.appendChild(thead);
+          table.appendChild(tbody);
+          trackCircuits2023.innerHTML = ''; // Clear any previous content
+          trackCircuits2023.appendChild(table);
+  
+        });
+    })
+   
+    // Function to create a card for a track
+    function createTrackCircuitCards(track){
+
+      // div element class "card-wrap"
+      const cardWrap = document.createElement("div");
+      cardWrap.className = "card-wrap";
+
+      // div element class "card"
+      const card = document.createElement("div");
+      card.className = "card";
+
+      // div element class "card-content z-index"
+      const cardContent = document.createElement("div");
+      cardContent.className = "card-content z-index";
+
+      // img element for the icon
+      const iconImg = document.createElement("img");
+      iconImg.className = "icon";
+      iconImg.src = track.race_img;
+      iconImg.alt = "";
+
+      // h3 element for the title
+      const title = document.createElement("h3");
+      title.className = "title-sm";
+      title.textContent = track.race_track;
+
+      // paragraph element for the text
+      const text = document.createElement("p");
+      text.className = "text";
+      text.textContent =track.race_date;
+
+      //  anchor element for the "Read more" button
+      const viewTrackCircuitBtn = document.createElement("a");
+      viewTrackCircuitBtn.href = "";
+      viewTrackCircuitBtn.className = "btn small";
+      viewTrackCircuitBtn.textContent= track.race_name;
+    
+      // Append the elements to their respective parent elements
+      cardContent.appendChild(iconImg);
+      cardContent.appendChild(title);
+      cardContent.appendChild(text);
+      cardContent.appendChild(viewTrackCircuitBtn);
+
+      card.appendChild(cardContent);
+      cardWrap.appendChild(card);
+    
+      return cardWrap;
+    }
+
+    // Function to view race details
+    function viewTrackCircuitCards(raceData) {
+      // Create a container with two columns
+      const containerGrid2 = document.createElement("div");
+      containerGrid2.className = "container grid-2";
+    
+      // Create the first column
+      const column1 = document.createElement("div");
+      column1.className = "column-1";
+    
+      // Create the second column for the image (you can set the image source here)
+      const column2Image = document.createElement("div");
+      column2Image.className = "column-2 image";
+    
+      // Create an h1 element for the race track name
+      const trackName = document.createElement("h1");
+      trackName.className = "header-title";
+      trackName.textContent = raceData.race_track;
+    
+      // Create a paragraph element for the race date
+      const raceDate = document.createElement("p");
+      raceDate.className = "text";
+      raceDate.textContent = raceData.race_date;
+
+     // Create a paragraph element for the fastest lap driver
+     const fastestLapName = document.createElement("p");
+     fastestLapName.className = "textStrong";
+     fastestLapName.textContent = `Fastest Lap : ${raceData.Fastest_lap.time}`;
+
+     // Create a paragraph element for the fastest lap team
+     const fastestLapTeam = document.createElement("p");
+     fastestLapTeam.className = "textStrong";
+     fastestLapTeam.textContent = `Driver : ${raceData.Fastest_lap.driverName}`;
+
+     // Create a paragraph element for the fastest lap time
+     const fastestLapTime = document.createElement("p");
+     fastestLapTime.className = "textStrong";
+     fastestLapTime.textContent = raceData.Fastest_lap.teamName;
+    
+      // Create a table for displaying race results
+      const raceResultTable = document.createElement("table");
+      raceResultTable.className = "styled-table";
+    
+      // Create a table header row
+      const tableHeaderRow = document.createElement("tr");
+    
+      // Add table header cells
+      const tableHeaderPositions = ["Position", "Driver", "Laps", "Time", "Points"];
+      tableHeaderPositions.forEach((headerText) => {
+        const tableHeaderCell = document.createElement("th");
+        tableHeaderCell.textContent = headerText;
+        tableHeaderRow.appendChild(tableHeaderCell);
+      });
+    
+      // Append the table header row to the table
+      raceResultTable.appendChild(tableHeaderRow);
+    
+      // Loop through and add race result rows
+      raceData.raceResults.forEach((result) => {
+        const resultRow = document.createElement("tr");
+    
+        // Add table data cells
+        const tableDataValues = [
+          result.position,
+          result.driverName,
+          result.lapsCompleted,
+          result.timeIndex,
+          result.pointsAwarded,
+        ];
+    
+        tableDataValues.forEach((value) => {
+          const tableDataCell = document.createElement("td");
+          tableDataCell.textContent = value;
+          resultRow.appendChild(tableDataCell);
+        });
+    
+        // Append the race result row to the table
+        raceResultTable.appendChild(resultRow);
+      });
+    
+      // Append the elements to their respective parent elements
+      column1.appendChild(trackName);
+      column1.appendChild(raceDate);
+      column1.appendChild(fastestLapName);
+      column1.appendChild(fastestLapTeam);
+      column1.appendChild(fastestLapTime);
+      
+      column2Image.appendChild(raceResultTable);
+    
+      containerGrid2.appendChild(column1);
+      containerGrid2.appendChild(column2Image);
+    
+      return containerGrid2;
+    }
+    
+
+    //function to handle view Track.
+    function handleClickViewCircuit(raceData){
+      
+        if (raceData) {
+          trackCircuits2023.innerHTML = '';
+          const F1TrackDetails = viewTrackCircuitCards(raceData);
+          console.log(raceData)
+          trackCircuits2023.appendChild(F1TrackDetails);
+        } else {
+          alert('Race not found.');
+        }
+    }
+
+    // Function to create a card for f1 Teams
+    function createTeamCards(team){
+
+      // div element class "card-wrap"
+      const cardWrap = document.createElement("div");
+      cardWrap.className = "card-wrap";
+
+      // div element class "card"
+      const card = document.createElement("div");
+      card.className = "cardTeam";
+
+      // div element class "card-content z-index"
+      const cardContent = document.createElement("div");
+      cardContent.className = "card-content z-index";
+
+      // img element for the icon
+      // const iconImg = document.createElement("img");
+      // iconImg.className = "icon";
+      // iconImg.src = track.race_img;
+      // iconImg.alt = "";
+
+      // h3 element for the title
+      const title = document.createElement("h3");
+      title.className = "title-sm";
+      title.textContent = team.teamName;
+
+      // paragraph element for the text
+      const text = document.createElement("p");
+      text.className = "text";
+      text.textContent =`Major Sponsor: ${team.majorSponsor} `;
+
+      //  anchor element for the "Read more" button
+      const countryBase = document.createElement("a");
+      countryBase.href = "";
+      // viewTrackCircuitBtn.className = "btn small";
+      countryBase.textContent= `Country: ${team.country} `;
+    
+      // Append the elements to their respective parent elements
+      // cardContent.appendChild(iconImg);
+      cardContent.appendChild(title);
+      cardContent.appendChild(text);
+      cardContent.appendChild(countryBase);
+
+      card.appendChild(cardContent);
+      cardWrap.appendChild(card);
+    
+      return cardWrap;
+    }
+
+    //initialize function
+    getRaces();
+
+}
+
+
+document.addEventListener("DOMContentLoaded", init)
